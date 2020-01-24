4aa3158346b5818938ca7046ba1ee1d3dbce4f7f
Devansh Sood <devanshsood.ss@gmail.com>
2020-01-24 22:28:19 +0530

    merge sort

---
+++ Arrays/merge_sort.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+void merge(int *a,int *l,int nl,int *r,int nr)
+{
+    int i,j,k=0;
+    while(i<nl && j<nr)
+    {
+        if(l[i]<r[j])
+        {
+            a[k]=l[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            a[k]=r[j];
+            k++;
+            j++;
+        }
+    }
+    while(i<nl)
+    {
+        a[k]=l[i];
+        k++;
+        i++;
+    }
+    while(j<nr)
+    {
+        a[k]=r[j];
+        k++;
+        j++;
+    }
+}
+
+void mergesort(int *a,int n)
+{
+    int mid,i;
+    mid=n/2;
+    int left[mid],right[n-mid];
+    if(n<2)
+    return ;
+    for(i=0;i<mid;i++)
+    {
+        left[i]=a[i];
+    }
+    int c=0;
+    for(i=mid;i<n;i++)
+    {
+        right[c]=a[i];
+        c++;
+    }
+    mergesort(left,mid);
+    mergesort(right,n-mid);
+    merge(a,left,mid,right,n-mid);
+}
+
+void printArray(int *a,int n)
+{
+    int i=0;
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+
+int main() 
+{ 
+    int arr[] = {12, 11, 13, 5, 6, 7}; 
+    int arr_size = sizeof(arr)/sizeof(arr[0]); 
+  
+    printf("Given array is \n"); 
+    printArray(arr, arr_size); 
+  
+    mergesort(arr,arr_size); 
+  
+     printf("\nSorted array is \n"); 
+     printArray(arr, arr_size); 
+    return 0; 
+} 
+
