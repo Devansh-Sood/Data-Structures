0ca857e84460eae7562bf322271bacbef24e5bc6
Devansh Sood <devanshsood.ss@gmail.com>
2020-03-02 23:20:59 +0530

    level order of tree

---
+++ Trees/level_order_traversal.c
@@ -0,0 +1,75 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+
+struct tnode
+{
+  int data;
+  struct tnode *left;
+  struct tnode *right;
+};
+
+struct tnode *newtNode(int d)
+{
+    struct tnode *temp= (struct tnode *)malloc(sizeof(struct tnode));
+    temp->data=d;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+
+int height(struct tnode *root)
+{
+    if(root==NULL)
+    return 0;
+    int l=height(root->left);
+    int r=height(root->right);
+    if(l>r)
+    return (l+1);
+    else
+    return (r+1);
+}
+
+/* Print nodes at a given level */
+void printGivenLevel(struct tnode* root, int level) 
+{ 
+    if (root == NULL) 
+        return; 
+    if (level == 1) 
+        printf("%d ", root->data); 
+    else if (level > 1) 
+    { 
+        printGivenLevel(root->left, level-1); 
+        printGivenLevel(root->right, level-1); 
+    } 
+}
+
+/* Function to print level order traversal a tree*/
+void printLevelOrder(struct tnode* root) 
+{ 
+    int h = height(root); 
+    int i; 
+    for (i=1; i<=h; i++) 
+        printGivenLevel(root, i); 
+} 
+
+/* Driver program to test above functions*/
+int main() 
+{ 
+  
+  /* Constructed binary tree is 
+            1 
+          /   \ 
+        2      3 
+      /  \ 
+    4     5 
+  */
+  struct tnode *root = newtNode(1); 
+  root->left        = newtNode(2); 
+  root->right       = newtNode(3); 
+  root->left->left  = newtNode(4); 
+  root->left->right = newtNode(5);  
+  
+   printLevelOrder(root);
+  
+  return 0; 
+}
